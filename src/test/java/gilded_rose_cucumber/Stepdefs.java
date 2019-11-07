8fcc37fb992b68f27178a4271667ef60de260c14
Emerson Mills <mrsn@odd-e.com>
2019-11-07 10:25:32 -0800

    added pom for springboot

--- src/test/java/gilded_rose_cucumber/Stepdefs.java
+++ src/test/java/gilded_rose_cucumber/Stepdefs.java
@@ -7,5 +7,35 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+    @Given("There are items in the catalog")
+    public void there_are_items_in_the_catalog() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
+    @When("I look at the item summary")
+    public void i_look_at_the_item_summary() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I see that all of the items have a sell by date")
+    public void i_see_that_all_of_the_items_have_a_sell_by_date() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I see that all the items have a value assigned to them")
+    public void i_see_that_all_the_items_have_a_value_assigned_to_them() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("it's the day <days_passed>")
+    public void itSTheDayDays_passed() {
+    }
+
+    @Then("then <item> have appropriately decreased <sell_by> and <value>")
+    public void thenItemHaveAppropriatelyDecreasedSell_byAndValue() {
+    }
 }
--- src/test/resources/features/inventory.feature
+++ src/test/resources/features/inventory.feature
@@ -1,5 +1,21 @@
 Feature: The Gilded Rose has inventory
-  Scenario: There's some items in the catalog
-    Given there are two items in the catalog
+  Scenario: All Items have a SellIn date that shows when the item should be sold by
+    Given There are items in the catalog
     When  I look at the item summary
-    Then  I see the total number of items is two
+    Then  I see that all of the items have a sell by date
+
+  Scenario: All Items have values
+      Given There are items in the catalog
+      When  I look at the item summary
+      Then I see that all the items have a value assigned to them
+
+  Scenario Outline: When a day passes items degrade
+      Given There are items in the catalog
+      When it's the day <days_passed>
+      Then then <item> have appropriately decreased <sell_by> and <value>
+    Examples:
+      | item    | days_passed |sell_by  | value |
+      | brie    | 1           |10       | $3.99 |
+      | cheddar | 1           |9        | $2.99 |
+      | brie    | 2           |9        | $2.99 |
+      | cheddar | 2           |8        | $1.99 |
