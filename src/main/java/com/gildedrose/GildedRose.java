79b01ac6b2beacc6cafa1a58a2e1f22cad1cac42
Matthew Plavcan <mplavcan@odd-e.com>
2019-11-21 00:32:12 -0800

    Extracted quality update methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,5 @@
 package com.gildedrose;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.*;
 
 public class GildedRose {
@@ -13,61 +11,64 @@ public class GildedRose {
 
     // this constructor is here so that tests can inject
     // items for testing after instantiation
-
     public GildedRose(){}
 
     public void updateQuality() {
-        ListIterator<Item> itemIterator = items.listIterator();
-        while (itemIterator.hasNext()) {
-            Item item = itemIterator.next();
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.updateQuality(item.quality - 1);
-                    }
-                }
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
+
+    private void updateItemQuality(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseItemQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            increaseQualityForBackstagePasses(item);
+        } else {
+            decreaseItemQuality(item);
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.updateSellIn( item.sellIn - 1);
+        }
+
+        if (item.sellIn < 0) {
+            if (item.name.equals("Aged Brie")) {
+                increaseItemQuality(item);increaseItemQuality(item);
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.updateQuality(0);
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                decreaseItemQuality(item);
+            }
+        }
+    }
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.updateQuality(item.quality + 1);
-                            }
-                        }
+    private void increaseQualityForBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    increaseItemQuality(item);
+                }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.updateQuality(item.quality + 1);
-                            }
-                        }
-                    }
+                if (item.sellIn < 6) {
+                    increaseItemQuality(item);
                 }
             }
+        }
+    }
 
+    private void decreaseItemQuality(Item item) {
+        if (item.quality > 0) {
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.updateSellIn( item.sellIn - 1);
+                item.updateQuality(item.quality - 1);
             }
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.updateQuality(item.quality - 1);
-                            }
-                        }
-                    } else {
-                        item.updateQuality( item.quality - item.quality);
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.updateQuality(item.quality + 1);
-                    }
-                }
-            }
+    private void increaseItemQuality(Item item) {
+        if (item.quality < 50) {
+            item.updateQuality(item.quality + 1);
         }
     }
 
